0d8ca59def93e2f4eac8eb72fdb1a395092368f4
Anton Tumasov <multibutterbread@gmail.com>
2015-11-02 14:51:46 +0300

    4th lab

---
+++ src/main/java/herzen/spb/App.java
@@ -0,0 +1,13 @@
+package herzen.spb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
